cf9f4fda83da4f61df914f90f7cd8fb7cbbc3bcd
Nimisha Asthagiri <nasthagiri@edx.org>
2016-04-25 11:10:35 -0400

    Update grading to use Block Transformers
    TNL-4455

--- lms/djangoapps/courseware/tests/test_views.py
+++ lms/djangoapps/courseware/tests/test_views.py
@@ -1337,7 +1337,7 @@ class ProgressPageTests(ModuleStoreTestCase):
         self.assertContains(resp, u"Download Your Certificate")
 
     @ddt.data(
-        *itertools.product(((55, 4, True), (55, 4, False)), (True, False))
+        *itertools.product(((33, 4, True), (33, 4, False)), (True, False))
     )
     @ddt.unpack
     def test_query_counts(self, (sql_calls, mongo_calls, self_paced), self_paced_enabled):
